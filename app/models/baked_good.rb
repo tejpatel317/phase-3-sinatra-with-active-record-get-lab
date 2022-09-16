53a85d85ca6ccbc42feb8312f1623f96d9f50d9d
Tej Patel <tejpatel317@gmail.com>
2022-09-16 19:29:05 -0400

    commit tree 164e1608e96c03f565a3a1842da51a8ee63cd457

--- app/models/baked_good.rb
+++ app/models/baked_good.rb
@@ -1,3 +1,11 @@
 class BakedGood < ActiveRecord::Base
-  # add association macro here
+  belongs_to :bakery
+
+  def self.by_price
+    self.all.order(price: :desc)
+  end
+
+  def self.expensive
+    by_price.first
+  end
 end
